0c35c73f80ffc2b64a3551feb08e3e877b8f354c
Dinesh Pinto <annual.fallout_0z@icloud.com>
2024-09-05 17:56:21 +0400

    fix: linter errors

--- src/tee_gemini/__init__.py
+++ src/tee_gemini/__init__.py
@@ -1,6 +1,6 @@
 from tee_gemini.gemini_api import GeminiAPI
 from tee_gemini.gemini_endpoint import GeminiEndpoint
-from tee_gemini.tpm_interface import TPMInterface
 from tee_gemini.main import start
+from tee_gemini.tpm_interface import TPMInterface
 
 __all__ = ["GeminiAPI", "GeminiEndpoint", "TPMInterface", "start"]
